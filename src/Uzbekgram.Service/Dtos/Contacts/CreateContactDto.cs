8c8274a252e89ce7a836873257d2eca5bee6c75d
A'zamjon Shaydullayev <azamjondev@gmail.com>
2023-02-07 14:11:34 +0500

    updated

---
+++ src/Uzbekgram.Service/Dtos/Contacts/CreateContactDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Uzbekgram.Service.Common.Attributes;
+
+namespace Uzbekgram.Service.Dtos.Contacts
+{
+    public class CreateContactDto
+    {
+        [Required, Integer]
+        public long FriendId { get; set; }
+    }
+}
---
+++ src/Uzbekgram.Service/Interfaces/Contacts/IContactService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Uzbekgram.Service.Dtos.Contacts;
+
+namespace Uzbekgram.Service.Interfaces.Contacts
+{
+    public interface IContactService
+    {
+        public Task<bool> CreateAsync(CreateContactDto createContactDto);
+    }
+}
