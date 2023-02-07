d2ffdeceb3240066f53c309a67415db513a1fe5b
A'zamjon Shaydullayev <azamjondev@gmail.com>
2023-02-07 14:13:55 +0500

    updated

--- src/Uzbekgram.Service/Dtos/Contacts/CreateContactDto.cs
+++ src/Uzbekgram.Service/Dtos/Contacts/CreateContactDto.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Uzbekgram.Domain.Entities.Contacts;
 using Uzbekgram.Service.Common.Attributes;
 
 namespace Uzbekgram.Service.Dtos.Contacts
@@ -12,5 +13,13 @@ namespace Uzbekgram.Service.Dtos.Contacts
     {
         [Required, Integer]
         public long FriendId { get; set; }
+
+        public static implicit operator Contact(CreateContactDto dto)
+        {
+            return new Contact()
+            {
+                FriendId = dto.FriendId,
+            };
+        }
     }
 }
